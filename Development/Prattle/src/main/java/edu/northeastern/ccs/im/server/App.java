ed462b5d470dee7183a3864f4353b14dc4c829be
Vaishnavi Hire <hire.v@husky.neu.edu>
2019-02-18 17:16:52 -0500

    sonarqube quality gate

--- Development/Prattle/src/main/java/edu/northeastern/ccs/im/server/App.java
+++ Development/Prattle/src/main/java/edu/northeastern/ccs/im/server/App.java
@@ -1,5 +1,6 @@
 package edu.northeastern.ccs.im.server;
 
+
 /**
  * Hello world!
  *
@@ -8,6 +9,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        System.out.print("Hello world");
+        // Server side code for Prattle
     }
 }
