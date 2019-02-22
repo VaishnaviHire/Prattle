ce28e296e701a474407b75e4282a440dbfc77f92
yashbhavsar <bhavsar.y@husky.neu.edu>
2019-02-22 12:48:58 -0500

    Some Other test added

--- Development/ChatServer/src/test/java/serverTest/ClientRunnableTest.java
+++ Development/ChatServer/src/test/java/serverTest/ClientRunnableTest.java
@@ -1,14 +1,22 @@
 package serverTest;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.channels.SocketChannel;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 import edu.northeastern.ccs.im.NetworkConnection;
 import edu.northeastern.ccs.im.server.ClientRunnable;
+import edu.northeastern.ccs.im.server.Prattle;
+import edu.northeastern.ccs.im.server.ServerConstants;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -17,7 +25,8 @@ public class ClientRunnableTest {
 
   private ClientRunnable clientRunnable;
   private SocketChannel socketChannel;
-  NetworkConnection connection;
+  @Mock
+  private NetworkConnection connection;
 
   @BeforeEach
   public void setup() {
@@ -25,13 +34,14 @@ public class ClientRunnableTest {
     try {
       socketChannel = SocketChannel.open();
       socketChannel.configureBlocking(false);
-      socketChannel.connect(new InetSocketAddress("localhost", 80));
+      socketChannel.connect(new InetSocketAddress("localhost", 4545));
 
     } catch (IOException e) {
       //do nothing
     }
     connection = new NetworkConnection(socketChannel);
     clientRunnable = new ClientRunnable(connection);
+
   }
 
   @Test
@@ -42,6 +52,22 @@ public class ClientRunnableTest {
 
   @Test
   public void testisInitialized() {
+    assertFalse(clientRunnable.isInitialized());
+  }
+
+  @Test
+  public void testRun() {
+    clientRunnable.run();
+    assertFalse(clientRunnable.isInitialized());
+  }
+
+  @Test
+  public void testTerminate() {
+    ScheduledExecutorService threadPool = Executors.newScheduledThreadPool(20);
+    ScheduledFuture<?> clientFuture = threadPool.scheduleAtFixedRate(clientRunnable, 200,
+            200, TimeUnit.MILLISECONDS);
+    clientRunnable.setFuture(clientFuture);
+    clientRunnable.terminateClient();
     assertFalse(clientRunnable.isInitialized());
   }
 }
