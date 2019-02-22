8e7b994950847bb08d354aeee4cea3a8a38290f1
yashbhavsar <bhavsar.y@husky.neu.edu>
2019-02-22 10:49:08 -0500

    test for clientRunnable, ClientTimer, Prattle

---
+++ Development/ChatServer/src/test/java/serverTest/ClientTimerTest.java
@@ -0,0 +1,27 @@
+package serverTest;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import edu.northeastern.ccs.im.server.ClientTimer;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class ClientTimerTest {
+
+  private ClientTimer clientTimer;
+
+  @BeforeEach
+  public void setup() {
+    clientTimer = new ClientTimer();
+  }
+
+  @Test
+  public void testisBehind() {
+    assertFalse(clientTimer.isBehind());
+  }
+
+  @Test
+  public void test2() {
+  }
+}
