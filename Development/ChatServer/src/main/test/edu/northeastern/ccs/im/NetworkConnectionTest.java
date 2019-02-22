57fa65234bc03f7bc87cc9e8c451681014874aeb
shivampatel8 <patel.shivam2@husky.neu.edu>
2019-02-21 20:22:27 -0500

    Branch for Jira Unit test story

---
+++ Development/ChatServer/src/main/test/edu/northeastern/ccs/im/NetworkConnectionTest.java
@@ -0,0 +1,13 @@
+package edu.northeastern.ccs.im;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NetworkConnectionTest {
+
+  @Test
+  void sendMessage() {
+
+  }
+}
