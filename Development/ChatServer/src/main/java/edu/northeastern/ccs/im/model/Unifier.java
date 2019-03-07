3f6a2124196fc15ae46585bf2c5f165a45f8afc5
Vaishnavi Hire <hire.v@husky.neu.edu>
2019-03-07 18:04:07 -0500

    MSD101SP19-23: Defined user and group models

---
+++ Development/ChatServer/src/main/java/edu/northeastern/ccs/im/model/Unifier.java
@@ -0,0 +1,11 @@
+package edu.northeastern.ccs.im.model;
+
+/**
+ *  Parent class of Users and Groups
+ */
+public interface Unifier {
+
+    public String getName();
+    public void setName(String name);
+    public String getType();
+}
--- Development/ChatServer/src/main/java/edu/northeastern/ccs/im/model/User.java
+++ Development/ChatServer/src/main/java/edu/northeastern/ccs/im/model/User.java
@@ -1,4 +1,60 @@
 package edu.northeastern.ccs.im.model;
 
-public interface User {
+/**
+ * Class defining user model
+ */
+public class User implements Unifier {
+
+    private String username;
+    private String userRole;
+
+    User(String username){
+        // Modify the constructor to create user with clientRunnable
+        this.username = username;
+        this.userRole = "plain_user";
+    }
+
+    /**
+     *
+     * @return the role of user as a plain user or admin
+     */
+    public String  getUserRole(){
+        return userRole;
+    }
+
+    /**
+     *
+     * @param role set user to specified role
+     */
+    public void  setUserRole(String role){
+         this.userRole = role;
+    }
+
+    /**
+     *
+     * @return method to get username
+     */
+    @Override
+    public String getName() {
+        return username;
+    }
+
+    /**
+     *
+     * @param name method to set given name to user
+     */
+    @Override
+    public void setName(String name) {
+        this.username = name;
+
+    }
+
+    /**
+     *
+     * @return the type of unifier
+     */
+    @Override
+    public String getType() {
+        return "user";
+    }
 }
