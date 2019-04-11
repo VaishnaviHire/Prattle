e8f9b98722f5e0b80b223aee2da807fcd0019c76
shivampatel18 <patel.shivam2@husky.neu.edu>
2019-04-11 03:04:15 -0400

    Added comments and fixed group and group1 problems and removed unifier interface

--- Development/ChatServer/src/main/java/edu/northeastern/ccs/im/server/ServerConstants.java
+++ Development/ChatServer/src/main/java/edu/northeastern/ccs/im/server/ServerConstants.java
@@ -5,37 +5,51 @@ package edu.northeastern.ccs.im.server;
  * version of the server spawns a new thread to handle each client that connects
  * to it. At this point, messages are broadcast to all of the other clients. It
  * does not send a response when the user has gone off-line.
- * 
+ * <p>
  * This work is licensed under the Creative Commons Attribution-ShareAlike 4.0
  * International License. To view a copy of this license, visit
  * http://creativecommons.org/licenses/by-sa/4.0/. It is based on work
  * originally written by Matthew Hertz and has been adapted for use in a class
  * assignment at Northeastern University.
- * 
+ *
  * @version 1.3
  */
 public class ServerConstants {
 
-	/** The port number to listen on. */
-	protected static final int PORT = 4545;
+    /**
+     * The port number to listen on.
+     */
+    protected static final int PORT = 4545;
 
-	/** The host to listen on. */
-	protected static final String HOST = "localhost";
+    /**
+     * The host to listen on.
+     */
+    protected static final String HOST = "localhost";
 
-	/** Amount of time we should wait for a signal to arrive. */
-	protected static final int DELAY_IN_MS = 50;
+    /**
+     * Amount of time we should wait for a signal to arrive.
+     */
+    protected static final int DELAY_IN_MS = 50;
 
-	/** Number of threads available in our thread pool. */
-	protected static final int THREAD_POOL_SIZE = 20;
+    /**
+     * Number of threads available in our thread pool.
+     */
+    protected static final int THREAD_POOL_SIZE = 20;
 
-	/** Delay between times the thread pool runs the client check. */
-	protected static final int CLIENT_CHECK_DELAY = 200;
+    /**
+     * Delay between times the thread pool runs the client check.
+     */
+    protected static final int CLIENT_CHECK_DELAY = 200;
 
-	/** Name of the private user who broadcasts interesting responses. */
-	protected static final String SERVER_NAME = "Prattle";
+    /**
+     * Name of the private user who broadcasts interesting responses.
+     */
+    protected static final String SERVER_NAME = "Prattle";
 
-	/** Name of the private user who handles bad requests. */
-	public static final int BOUNCER_ID = -999;
+    /**
+     * Name of the private user who handles bad requests.
+     */
+    public static final int BOUNCER_ID = -999;
 
 	/** Private constructor to prevent anyone from creating one of these. */
 	private ServerConstants() {
